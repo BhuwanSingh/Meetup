1737d32bbef869bcaf5564bb4b0dd25bb4518b44
Bhuwan <bhuwankumar2000@gmail.com>
2021-09-28 21:54:13 +0530

    final 2

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,15 @@
+import { useContext } from "react";
+
 import { Link } from "react-router-dom";
 import classes from "./MainNavigation.module.css";
+import FavouritesContext from "../../store/favourites-contest";
 
 import Logo from './logo.png';
 
 
 const MainNavigation = () => {
+  const favouriteCtx = useContext(FavouritesContext);
+
   return (
     <header className={classes.header}>
       <div><Link to="/"><img src={Logo} height="200px" alt="" /></Link></div>
@@ -14,7 +19,9 @@ const MainNavigation = () => {
             <Link to="/">AllMeetups</Link>
           </li>
           <li>
-            <Link to="/Favourites">Favourites</Link>
+            <Link to="/Favourites">Favourites
+            <span className={classes.badge}>{favouriteCtx.totalFavourites}</span>
+            </Link>
           </li>
           <li>
             <Link to="/NewMeetup">NewMeetup</Link>
